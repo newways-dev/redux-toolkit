0074affe6afc669cc4504ae92920a1121e8af703
newways-dev <reactwebdew@gmail.com>
2023-03-19 17:05:00 +0300

    first commit

---
+++ 02_app-structure-data-flow/src/App.js
@@ -0,0 +1,13 @@
+import Posts from './features/posts/Posts'
+import AddPostForm from './features/posts/AddPostForm'
+
+function App() {
+  return (
+    <main className='App'>
+      <AddPostForm />
+      <Posts />
+    </main>
+  )
+}
+
+export default App
